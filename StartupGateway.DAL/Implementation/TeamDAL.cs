4de859991e91b0fba63222102d4675795ab031f9
Ibrahim Hakam <77873485+ibrahimtheboss@users.noreply.github.com>
2024-03-19 12:36:16 +0530

    Rest of (Repository) DAL and interface Added
    Rest of (Repository) DAL and interface has been Added

---
+++ StartupGateway.DAL/Implementation/TeamDAL.cs
@@ -0,0 +1,24 @@
+﻿/**
+ * Created by: Ibrahim
+ * Created on: 19/03/2024
+ * Description:  TeamDAL [Data Access Layer] Class (Repository).
+ * 
+ * */
+
+using StartupGateway.DAL.Interfaces;
+using StartupGateway.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartupGateway.DAL.Implementation
+{
+    public class TeamDAL : BaseDAL<Team>, ITeamDAL
+    {
+        public TeamDAL(DataContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ StartupGateway.DAL/Interfaces/ICollaborateDAL.cs
@@ -0,0 +1,20 @@
+﻿/**
+ * Created by: Ibrahim
+ * Created on: 19/03/2024
+ * Description: Interface ICollaborateDAL (Repository).
+ * 
+ * */
+
+using StartupGateway.BusinessEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartupGateway.DAL.Interfaces
+{
+    public interface ICollaborateDAL: IBaseDAL<Collaborate>
+    {
+    }
+}
