064c9e4aa9b34044100cdb36364a00745bd3b2e6
ZuhriAshroff <zuhriashroff7@gmail.com>
2024-03-19 12:15:18 +0530

    UoW Implementation (Awaiting Testing)

---
+++ StartupGateway.DAL/Implementation/UoW.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using StartupGateway.DAL;
+
+namespace StartupGateway.UoW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataContext _context;
+        private readonly Dictionary<Type, object> _repositories;
+
+        public UnitOfWork(DataContext context)
+        {
+            _context = context;
+            _repositories = new Dictionary<Type, object>();
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Rollback()
+        {
+            // Rollback changes if needed
+        }
+
+        public T GetRepository<T>() where T : class
+        {
+            if (_repositories.ContainsKey(typeof(T)))
+            {
+                return (T)_repositories[typeof(T)];
+            }
+
+            T repository = (T)Activator.CreateInstance(typeof(T), _context);
+            _repositories.Add(typeof(T), repository);
+            return repository;
+        }
+      
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ StartupGateway.DAL/Interfaces/IUoW.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StartupGateway.UoW.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    { 
+        void Commit();
+        void Rollback();
+        T GetRepository<T>() where T : class;
+    }
+}
