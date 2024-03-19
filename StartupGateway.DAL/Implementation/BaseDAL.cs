69382211939768afa6b7b4d47bfd79902b7fc45b
zaid nilawfer <zaidnilawfer@gmail.com>
2024-03-19 11:04:15 +0530

    Base Data access layer done

---
+++ StartupGateway.DAL/Implementation/BaseDAL.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StartupGateway.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartupGateway.DAL.Implementation
+{
+    public class BaseDAL<TEntity> : IBaseDAL<TEntity> where TEntity : class
+    {
+        private readonly DataContext _context;
+
+        public BaseDAL(DataContext context)
+        {
+            _context = context;
+        }
+        /// <summary>
+        /// Add a new Record to the database.
+        /// </summary>
+        /// <param name="entity">The entity to add.</param>
+        public void AddEntity(TEntity entity)
+        {
+            _context.Add(entity);
+        }
+        /// <summary>
+        /// Retrieve all Records from the database.
+        /// </summary>
+        /// <returns>A collection of all projects.</returns>
+        public IEnumerable<TEntity> GetAllRecords()
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+
+        /// <summary>
+        /// Get a Record by its ID.
+        /// </summary>
+        /// <param name="projectId">The ID of the Record to retrieve.</param>
+        /// <returns>The Record with the specified ID.</returns>
+        public TEntity GetEntityById(int entityId)
+        {
+            return _context.Set<TEntity>().Find(entityId);
+        }
+
+        /// <summary>
+        /// Get a Record by name using a predicate.
+        /// </summary>
+        /// <param name="predicate">The predicate to match the Record by name.</param>
+        /// <returns>The Record that matches the predicate.</returns>
+       
+        public TEntity GetEntityByName(Func<TEntity, bool> predicate)
+        {
+            return _context.Set<TEntity>().FirstOrDefault(predicate);
+        }
+        /// <summary>
+        /// Update an existing Record in the database.
+        /// </summary>
+        /// <param name="entity">The updated project entity.</param>
+
+        public void UpdateEntity(TEntity entity)
+        {
+            if (_context.Set<TEntity>().Local.Any(e => e == entity))
+            {
+                _context.Set<TEntity>().Attach(entity);
+            }
+
+            // Mark the entity as modified to ensure it's updated in the database
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        /// <summary>
+        /// Dispose of the database context to free resources.
+        /// </summary>
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        /// <summary>
+        /// Commit changes to the database.
+        /// </summary>
+        public void CommitChanges()
+        {
+            _context.SaveChanges();
+        }
+
+    }
+}
---
+++ StartupGateway.DAL/Interfaces/IBaseDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartupGateway.DAL.Interfaces
+{
+    public interface IBaseDAL<TEntity>:IDisposable where TEntity : class
+    {
+        IEnumerable<TEntity> GetAllRecords();
+        TEntity GetEntityById(int id);
+        void AddEntity(TEntity entity);
+        TEntity GetEntityByName(Func<TEntity, bool> predicate);
+        void UpdateEntity(TEntity entity);
+        void CommitChanges();
+    }
+}
